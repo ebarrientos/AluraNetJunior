01787f96ce3dec1004cb430c376ee0da1df86b18
Erico Barrientos <erico.barrientos@gmail.com>
2019-09-27 00:24:46 -0300

    Adcionado modelo Categoria

---
+++ CasaDoCodigo/Models/ViewModels/CategoriaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models.ViewModels
+{
+    public class CategoriaViewModel
+    {
+        public CategoriaViewModel(IList<Produto> itens)
+        {
+            Itens = itens.Select(produto => produto.Categoria).Distinct(CategoriaEqualityComparer.Instancia).ToList();
+        }
+
+        public IList<Categoria> Itens { get; }
+        public string Search { get; set; }
+    }
+}
